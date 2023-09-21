5f3c7b493b86d7984347121c2fb23a5282e93818
Brian Sajeev <briansajeev@gmail.com>
2023-09-22 01:07:44 +0530

    added binaries for undo_move error debugging log

--- src/engine.cpp
+++ src/engine.cpp
@@ -362,35 +362,31 @@ void Engine::find_best_move(const Board &b)
     }
 }
 
-// #include <algorithm>
-// #include <random>
-// #include <iostream>
-
-// #include "board.hpp"
-// #include "engine.hpp"
-
-// void Engine::find_best_move(const Board& b) {
+// void Engine::find_best_move(const Board &b)
+// {
 
 //     // pick a random move
 
 //     auto moveset = b.get_legal_moves();
-//     if (moveset.size() == 0) {
+//     if (moveset.size() == 0)
+//     {
 //         this->best_move = 0;
 //     }
-//     else {
+//     else
+//     {
 //         std::vector<U16> moves;
-//         // std::cout << all_boards_to_str(b) << std::endl;
-//         for (auto m : moveset) {
-//             // std::cout << move_to_str(m) << " ";
+//         std::cout << all_boards_to_str(b) << std::endl;
+//         for (auto m : moveset)
+//         {
+//             std::cout << move_to_str(m) << " ";
 //         }
-//         // std::cout << std::endl;
+//         std::cout << std::endl;
 //         std::sample(
 //             moveset.begin(),
 //             moveset.end(),
 //             std::back_inserter(moves),
 //             1,
-//             std::mt19937{std::random_device{}()}
-//         );
+//             std::mt19937{std::random_device{}()});
 //         this->best_move = moves[0];
 //     }
 // }
